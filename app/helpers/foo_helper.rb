42695de7fd28f8f7c34d326b611c8780b2d2cf5a
VOLODYMYR <v.n.zubarev@gmail.com>
2018-11-19 10:25:16 +0200

    Initial commit: RoR app was builded. Login, Registration and Edit Profile Pages was created. Facebook Sign-Up and Sign-In was created. Email confomation for new users was created.Some small CSS style changes for buttons, font, navbar was maded.

---
+++ app/helpers/foo_helper.rb
@@ -0,0 +1,12 @@
+module FooHelper
+  def toastr_flash_class(type)
+    case type
+    when "alert"
+      "toastr.error"
+    when "notice"
+      "toastr.success"
+    else
+      "toastr.info"
+    end
+  end
+end
